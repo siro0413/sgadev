ddc25b73cf38fcd57345c6280378e5ed570c50bc
siro0413 <48557104+siro0413@users.noreply.github.com>
2019-08-29 03:07:57 +0900

    Add files via upload

---
+++ 190828 로그라이크/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <time.h>
+#include <Windows.h>
+#include <random>
+#include <stdio.h>
+#include <conio.h>
+
+#define SETPOS SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos)
+
+using namespace std;
+
